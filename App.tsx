2d6f0d9f0b4793b69a64acce7e918afce8e100a7
loressl <loryssl@hotmail.com>
2023-01-21 22:13:58 -0300

    feat: apply styles and settings in component Basket

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
 import { Basket } from './src/screens/Basket';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat'
 
 export default function App() {
+  const [fontLoading] = useFonts({
+    "MontSerratRegular": Montserrat_400Regular,
+    "MontSerratBold": Montserrat_700Bold
+  })
+
+  if(!fontLoading) {
+    return (
+      <View>Carregando...</View>
+    )
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/screens/Basket.tsx
+++ src/screens/Basket.tsx
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 import imageTop from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width
 
@@ -9,6 +10,21 @@ export function Basket() {
         <>
             <Image source={imageTop} style={styles.imageTop} />
             <Text style={styles.title}>Detalhe da cesta</Text>
+
+            <View style={styles.basket}>
+                <Text style={styles.name}>Cesta de Verduras</Text>
+                <View style={styles.farm}>
+                    <Image style={styles.imageFarm} source={logo} />
+                    <Text style={styles.nameFarm}>Jenny Jack Farm</Text>
+                </View>
+                <Text style={styles.description}>
+                    Uma cesta com produtos selecionados 
+                    cuidadosamente da fazenda direto para sua cozina
+                </Text>
+                <Text style={styles.price}>
+                    R$ 40,00
+                </Text>
+            </View>
         </>
     )
 }
@@ -28,5 +44,41 @@ const styles = StyleSheet.create({
         marginTop: '5%',
         fontWeight: 'bold',
         padding: 16
+    },
+    basket:{
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    name: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily: 'MontSerratBold'
+    },
+    farm: {
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+    imageFarm:{
+        width: 32,
+        height: 32
+    },
+    nameFarm: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: 'MontSerratRegular'
+    },
+    description: {
+        color: '#a3a3a3',
+        fontSize: 16,
+        lineHeight: 26
+    },
+    price: {
+        color: "#2a9f85",
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
